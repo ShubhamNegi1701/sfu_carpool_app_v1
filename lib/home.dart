0f8fee469029d9fefb08486240d9e84d436b070b
unknown <stevenzhu0811@gmail.com>
2020-07-06 23:01:09 -0700

    map page, location autosearch and location markers implemented

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,74 @@
-//import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart';
+import 'package:geocoder/geocoder.dart';
 
-class MainMapPage extends StatelessWidget{
+const GoogleApiKey = "AIzaSyDMIiuEny9d4SrnacqdU7-0_c8YgdvaVjg";
+const LatLng _vancouver = const LatLng(49.2827, -123.1207);
+
+GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: GoogleApiKey);
+
+Set<Marker> markers = Set();
+
+class MainMapPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: Flexible(
-        flex:2
-      )
+  Map createState() => Map();
+}
+
+class Map extends State<MainMapPage> {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: new Scaffold(
+        body: new Stack(
+            children: <Widget>[
+              GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                    target: _vancouver,
+                    zoom: 10.0,
+                  ),
+                  markers: markers
+              ),
+              Positioned(
+                  top: 10,
+                  right: 15,
+                  left: 15,
+                  child: Container(
+                      color: Colors.white,
+                      child: RaisedButton(
+                          onPressed: () async {
+                            Prediction p = await PlacesAutocomplete.show(
+                                context: context,
+                                apiKey: GoogleApiKey,
+                                mode: Mode.overlay,
+                                location: Location(49.2827, -123.1207),
+                                radius: 10000);
+                            addMarker(p);
+                          },
+                          child: new Text('Ride Now')
+                      )
+                  )
+              )
+            ]
+        ),
+      ),
     );
+  }
+
+  void addMarker(Prediction p) async {
+    if (p != null) {
+      PlacesDetailsResponse detail = await _places.getDetailsByPlaceId(
+          p.placeId);
+
+      double lat = detail.result.geometry.location.lat;
+      double lng = detail.result.geometry.location.lng;
+
+      setState(() {
+        markers.add(Marker(
+            markerId: MarkerId('test'),
+            position: LatLng(lat, lng)
+        ));
+      });
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:sfucarpoolapp/home.dart';
 
 import 'login.dart';
 import 'signup.dart';
@@ -41,37 +42,38 @@ class HomeButtons extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Text('SFU',
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+          Text('SFU',
             textAlign: TextAlign.left,
             style: TextStyle(
                 fontSize: 60, fontFamily: 'Roboto', color: Colors.white)),
-        Text('Carpool\n',
+          Text('Carpool\n',
             textAlign: TextAlign.left,
             style: TextStyle(
               fontSize: 60,
               fontFamily: 'Roboto',
               fontWeight: FontWeight.bold,
               color: Colors.white,
-            )),
-        ButtonTheme(
-          minWidth: 200,
-          height: 40,
-          child: RaisedButton(
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LogInPage()));
-            },
-            child: const Text('   LOG IN   ',
-                style: TextStyle(fontSize: 20, color: Colors.white)),
-            color: Colors.red[900],
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0)),
+            ),
           ),
-        ),
-        ButtonTheme(
+          ButtonTheme(
+            minWidth: 200,
+            height: 40,
+            child: RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LogInPage()));
+              },
+              child: const Text('   LOG IN   ',
+                  style: TextStyle(fontSize: 20, color: Colors.white)),
+              color: Colors.red[900],
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+            ),
+          ),
+          ButtonTheme(
             minWidth: 200,
             height: 40,
             child: RaisedButton(
@@ -85,9 +87,27 @@ class HomeButtons extends StatelessWidget {
               color: Colors.red[900],
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20.0)),
-            ))
-      ],
-    ));
+            ),
+          ),
+          ButtonTheme(
+            minWidth: 200,
+            height: 40,
+            child: RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainMapPage()));
+              },
+              child: const Text('   MAP PAGE   ',
+                  style: TextStyle(fontSize: 20, color: Colors.white)),
+              color: Colors.red[900],
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
